b9cd8f24cdf97e1100c7bf1cb0f95a10a5450ef2
Marc Matterson <marcmatterson@gmail.com>
2023-01-08 23:07:44 +0000

    changed logic for medium

--- src/streamlit-test.py
+++ src/streamlit-test.py
@@ -10,17 +10,15 @@ st.title("Gapminder Data Exploration")
 # Create a sidebar with various filters
 year_min = st.sidebar.slider("Year Min", 1952, 2007, 1952)
 year_max = st.sidebar.slider("Year Max", 1952, 2007, 2007)
-continent = st.sidebar.multiselect("Continent", df["continent"].unique())
+country = st.sidebar.multiselect("Country", df["country"].unique())
 log_y = st.sidebar.radio("Y-axis Scale", ["Linear", "Log"])
 
 # Display the selected options
-st.write("Continent(s) selected:", continent)
+# st.write("country(s) selected:", country)
 
 # Filter the dataset based on the selected values
 filtered_df = df[
-    (df["year"] >= year_min)
-    & (df["year"] <= year_max)
-    & df["continent"].isin(continent)
+    (df["year"] >= year_min) & (df["year"] <= year_max) & df["country"].isin(country)
 ]
 
 # Create a scatter plot of GDP per capita vs life expectancy
@@ -29,8 +27,13 @@ fig1 = px.scatter(
     x="gdpPercap",
     y="lifeExp",
     size="pop",
-    color="continent",
-    title="Life Expectancy vs GDP",
+    color="country",
+    title="Life Expectancy vs GDP per Capita",
+)
+
+# Update the layout to rename the axes
+fig1.update_layout(
+    xaxis=dict(title="GDP per Capita"), yaxis=dict(title="Life Expectancy")
 )
 
 # Set the y-axis scale to log if selected
@@ -42,38 +45,26 @@ st.plotly_chart(fig1)
 
 # Display some text explaining the chart
 st.markdown(
-    "This chart shows the relationship between GDP per capita and life expectancy for different countries and continents. You can use the filters in the sidebar to adjust the year range and continent, and to change the y-axis scale."
+    """
+    This chart shows the relationship between GDP per capita and 
+    life expectancy for different countries and countries. You can 
+    use the filters in the sidebar to adjust the year range and country, 
+    and to change the y-axis scale.
+    """
 )
 
+# # Create a line chart of population by year
+# fig2 = px.line(
+#     filtered_df, x="year", y="pop", color="country", title="Population by Year"
+# )
 
-# Create a bar chart of GDP per capita by continent
-fig2 = px.bar(
-    filtered_df, x="continent", y="gdpPercap", title="GDP per Capita by Continent"
-)
+# # Update the layout to rename the axes
+# fig2.update_layout(xaxis=dict(title="Year"), yaxis=dict(title="Population"))
 
-# Display the plot
-st.plotly_chart(fig2)
+# # Display the plot
+# st.plotly_chart(fig2)
 
-# Display some text explaining the chart
-st.markdown(
-    "This chart shows the GDP per capita for different continents. You can use the filters in the sidebar to adjust the year range and continent."
-)
-
-# Create a line chart of population by year
-fig3 = px.line(
-    filtered_df, x="year", y="pop", color="continent", title="Population by Year"
-)
-
-# Display the plot
-st.plotly_chart(fig3)
-
-# Display some text explaining the chart
-st.markdown(
-    "This chart shows the population for different continents over time. You can use the filters in the sidebar to adjust the year range and continent."
-)
-
-# Create a bar chart of the countries
-fig = px.bar(df, x="country", y="gdpPercap", title="GDP per Capita by Country")
-
-# Display the plot
-st.plotly_chart(fig)
+# # Display some text explaining the chart
+# st.markdown(
+#     "This chart shows the GDP per capita for different countries. You can use the filters in the sidebar to adjust the year range and country."
+# )
